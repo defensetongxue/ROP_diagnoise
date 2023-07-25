2c4cca6437fedd1472768d72aaae576772df473a
defense <1900013009@pku.edu.cn>
2023-07-25 16:15:45 +0800

    style

--- utils_/paser_ridge.py
+++ utils_/paser_ridge.py
@@ -1,13 +1,36 @@
 import  os
 import json
-def parse_json(input_data,label_class=0,image_dict="../autodl-tmp/images"):
+from .api_record import api_check,api_update
+def generate_ridge(json_dict,data_path):
+    print(f"begin paser ridge from {json_dict}")
+    with open(os.path.join(data_path,'annotations.json'),'r') as f:
+        original_annoation=json.load(f)
+    file_list=sorted(os.listdir(json_dict))
+    print(f"read the origianl json file from {file_list}")
+    for file in file_list:
+        if not file.split('.')[-1]=='json':
+            print(f"unexpected file {file} in json_src")
+            continue
+        if not file.split('.')[-1]=='json':
+            print(f"unexpected file {file} in json_src")
+            continue
+        with open(os.path.join(json,file), 'r') as f:
+            data = json.load(f)
+        if int(file[0])==6:
+            continue
+        for json_obj in data:
+            image_name,new_data=parse_json(json_obj)
+            if new_data["ridge_number"]>0:     
+                original_annoation[image_name].update(new_data)
+    api_update(data_path,'ridge',"lesion of ROP, including the coordinate lesion of the rop")
+    print("finished")
+    
+def parse_json(input_data):
     annotations = input_data.get("annotations", [])
     if annotations:
         result = annotations[0].get("result", [])
     image_name=input_data["file_upload"].split('-')[-1]
-    new_data = {
-        "image_name": image_name,
-        "image_path":os.path.join(image_dict,image_name),
+    new_data = {\
         "ridge_number": 0,
         "ridge_coordinate": [],
         "other_number": 0,
@@ -18,7 +41,6 @@ def parse_json(input_data,label_class=0,image_dict="../autodl-tmp/images"):
         "pre_plus_coordinate": [],
         "vessel_abnormal_number":0,
         "vessel_abnormal_coordinate":[],
-        "class": label_class
     }
 
     for item in result:
@@ -40,82 +62,8 @@ def parse_json(input_data,label_class=0,image_dict="../autodl-tmp/images"):
             elif label == "Pre-plus":
                 new_data["pre_plus_number"] += 1
                 new_data["pre_plus_coordinate"].append((x, y))
-
-            if label_class==3 and label =='Stage':
+            elif label =='Stage':
                 new_data["vessel_abnormal_number"]+=1
                 new_data['vessel_abnormal_coordinate'].append((x,y))
 
-    return new_data
-def parse_json_file(file_dict,data_path):
-    
-    annotation=[]
-    file_list=sorted(os.listdir(file_dict))
-    print(f"read the origianl json file from {file_list}")
-    for file in file_list:
-        if not file.split('.')[-1]=='json':
-            print(f"unexpected file {file} in json_src")
-            continue
-        if not file.split('.')[-1]=='json':
-            print(f"unexpected file {file} in json_src")
-            continue
-        with open(os.path.join(file_dict,file), 'r') as f:
-            data = json.load(f)
-        label_class=int(file[0])
-        if label_class==6:
-            continue
-        for json_obj in data:
-            new_data=parse_json(json_obj,label_class=label_class,
-                                image_dict=os.path.join(data_path,'images'))
-            if new_data["ridge_number"]>0:        
-                annotation.append(new_data)
-
-    return annotation
-
-def split_data(data_path, annotations):
-    # Important: do not shuffle, as there may be some images very similar be split into different set
-    os.makedirs(os.path.join(data_path, 'ridge'),exist_ok=True)
-    os.system(f"rm -rf {os.path.join(data_path, 'ridge')}/*")
-
-    with open(os.path.join(data_path, 'annotations', "train.json"), 'r') as f:
-        train_list=json.load(f)
-        train_list=[i['image_name'] for i in train_list]
-    with open(os.path.join(data_path, 'annotations', "val.json"), 'r') as f:
-        val_list=json.load(f)
-        val_list=[i['image_name'] for i in val_list]
-    with open(os.path.join(data_path, 'annotations', "test.json"), 'r') as f:
-        test_list=json.load(f)
-        test_list=[i['image_name'] for i in test_list]
-
-    train_annotations = []
-    val_annotations = []
-    test_annotations =[]
-    train_condition={'1':0,"2":0,"3":0}
-    val_condition={'1':0,"2":0,"3":0}
-    test_condition={'1':0,"2":0,"3":0}
-    for data in annotations:
-        if data['image_name'] in train_list:
-            train_annotations.append(data)
-            train_condition[str(data['class'])]+=1
-        if data['image_name'] in val_list:
-            val_annotations.append(data)
-            val_condition[str(data['class'])]+=1
-        if data['image_name'] in test_list:
-            test_annotations.append(data)
-            test_condition[str(data['class'])]+=1
-    with open(os.path.join(data_path, 'ridge', 'train.json'), 'w') as f:
-        json.dump(train_annotations, f, indent=2)
-
-    with open(os.path.join(data_path, 'ridge', 'val.json'), 'w') as f:
-        json.dump(val_annotations, f, indent=2)
-
-    with open(os.path.join(data_path, 'ridge', 'test.json'), 'w') as f:
-        json.dump(test_annotations, f, indent=2)
-
-    print(f"Total samples: {len(annotations)}"  )
-    print(f"Train samples: {len(train_annotations)} {train_condition} {train_condition}")
-    print(f"Validation samples: {len(val_annotations)} {val_condition} {val_condition}")
-    print(f"Test samples: {len(test_annotations)} {test_condition} {test_condition}")
-def generate_ridge(json_dict,data_path):
-    annotations=parse_json_file(json_dict,data_path)
-    split_data(data_path,annotations)
-    
+    return image_name,new_data
