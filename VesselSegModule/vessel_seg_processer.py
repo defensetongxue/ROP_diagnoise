fc519eb29e07e38ad9d2a9f97c0dc7fc254ff94e
guoyi <1900013009@pku.edu.cn>
2023-10-09 20:24:50 +0800

    add

--- VesselSegModule/vessel_seg_processer.py
+++ VesselSegModule/vessel_seg_processer.py
@@ -14,31 +14,42 @@ class ContrastEnhancement:
         enhancer = ImageEnhance.Contrast(img)
         img = enhancer.enhance(self.factor)
         return img
+class NormperImage():
+    def __init__(self,mask_tensor):
+        self.mask_tensor=mask_tensor.unsqueeze(0)
+    def __call__(self,n):
+        # Mask the image tensor where mask values are greater than 0
+        masked_values = n[self.mask_tensor > -1]
+        # Compute the min and max values from the masked tensor
+        min_val = torch.min(masked_values)
+        max_val = torch.max(masked_values)
 
+        # Normalize the image tensor using these min and max values
+        normalized_n = (n - min_val) / (max_val - min_val)
+        return normalized_n
+    
 class VesselSegProcesser():
     def __init__(self, 
                  model_dict="./ROP_diagnoise/model_save"):
         checkpoint = torch.load(
             os.path.join(model_dict,'vessel_seg.pth'))
         loaded_state_dict = checkpoint['state_dict']
-        new_state_dict = {k.replace("module.", ""): v for k, v in loaded_state_dict.items()}
         self.model = FR_UNet().cuda()
-        self.model.load_state_dict(new_state_dict)
+        self.model.load_state_dict(loaded_state_dict)
         self.model.eval()
         # generate mask
         mask = Image.open('./VesselSegModule/mask.png')
-        mask=transforms.Resize((300,400))(mask)
+        mask=transforms.Resize((800,800))(mask)
         mask = transforms.ToTensor()(mask)[0]
         self.mask = mask
 
         self.transforms = transforms.Compose([
             ContrastEnhancement(),
-            transforms.Resize((300,400)),
+            transforms.Resize((800,800)),
             transforms.Grayscale(1),
             transforms.ToTensor(),
-            transforms.Normalize([0.3968], [0.1980])
-            # the mean and std is cal by 12 rop1 samples
-            # TODO using more precise score
+            transforms.Normalize([0.31269], [0.166996]),
+            NormperImage(mask)
         ])
 
     def __call__(self, img_path,save_path=None):
@@ -51,6 +62,7 @@ class VesselSegProcesser():
             print(mask_part.shape)
             # mask
             predict = torch.where(self.mask < 0.1, self.mask, mask_part)
+            predict= torch.where(mask_part>0.01,torch.ones_like(mask_part),mask_part)
             if save_path:
                 cv2.imwrite(save_path,
                     np.uint8(predict.numpy()*255))
--- debug.py
+++ debug.py
@@ -1,5 +1,4 @@
 import torch,os
-from VesselSegModule.models import FR_UNet
 from VesselSegModule  import VesselSegProcesser
 # Load checkpoin
 processer=VesselSegProcesser('./model_save')
@@ -7,3 +6,4 @@ data_list=[f"{str(i+1)}.jpg" for i in range(40)]
 for image_name in data_list:
     processer(os.path.join('../autodl-tmp/dataset_ROP/images',image_name),
               os.path.join('./experiments',image_name))
+processer('./01_test.tif','1.jpg')
