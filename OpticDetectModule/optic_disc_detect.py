ac670ca0219d3fbe962b93a7f82bb7e3a51bb7e5
guoyi <1900013009@pku.edu.cn>
2023-10-31 19:04:14 +0800

    add

--- OpticDetectModule/optic_disc_detect.py
+++ OpticDetectModule/optic_disc_detect.py
@@ -22,18 +22,13 @@ def generate_optic_disc_location(data_path='./data',
     mask=Image.open('./OpticDetectModule/mask.png').convert('L')
     mask=np.array(mask)
     mask[mask>0]=1
-    new={}
-    cnt=0
     for image_name in data_dict:
-        image_name="5168.jpg"
-        cnt+=1
         data=data_dict[image_name]
-        new[image_name]={}
         coordinate,distance=processer(data['image_path'])
         if distance=='visible':
             coordinate=coordinate.tolist()
             coordinate=[int(i) for i in coordinate]
-            new[image_name]['optic_disc_pred']={
+            data_dict[image_name]['optic_disc_pred']={
             'position':coordinate,
             'distance':distance
             }
@@ -43,12 +38,12 @@ def generate_optic_disc_location(data_path='./data',
         point=[int(coordinate[0]),int(coordinate[1])]
         x,y=find_nearest_zero(mask,point)
         distance=class_model(data['image_path'])
-        new[image_name]['optic_disc_pred']={
+        data_dict[image_name]['optic_disc_pred']={
             'position':[x,y],
             'distance':distance}
 
-    with open(os.path.join(data_path,'optic_dict.json'),'w') as f:
-        json.dump(new,f)
+    with open(os.path.join(data_path,'annotations.json'),'w') as f:
+        json.dump(data_dict,f)
     
 
 def find_nearest_zero(mask, point):
@@ -75,4 +70,4 @@ def find_nearest_zero(mask, point):
         x += direction[0]
         y += direction[1]
 
-    return None
+    return int(x), int(y)
